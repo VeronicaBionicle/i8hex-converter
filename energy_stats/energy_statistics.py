cfd9665310f8e418a05407148de456e4e67db266
Veronica <veronikasuper1@gmail.com>
2020-04-21 17:26:59 +1000

    Added some lines of nominal line voltage and tried to interpolate

--- energy_stats/energy_statistics.py
+++ energy_stats/energy_statistics.py
@@ -1,43 +1,55 @@
 import os
 from utils import DATA_DIR, CHART_DIR
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+from matplotlib.pyplot import clf, plot, title, grid, gcf, gca, xlabel, xlim, xticks, ylabel, ylim, yticks, hlines, savefig, show, subplots_adjust, get_current_fig_manager
 import matplotlib.dates as mdates
 from datetime import datetime
 
 # func to decorate and plot graph
-def plot_graph(x, y, fname):
-    #global x_name
-   # global y_name
-    plt.clf()
-    # plot graph
-    plt.plot(x, y, linewidth=2, color='seagreen', linestyle='-', marker='.', picker=True)  
+def plot_graph(x, y, plot_standart = True, fname = None):
+    clf()
+    # plot graph from real line voltages
+    plot(x, y, linewidth=2, color='seagreen', linestyle='-', marker='.')
+    '''
+    #from scipy.interpolate import interp1d
+    # averaged
+    prep_x = x - min(x) 
+    f = interp1d(prep_x, y, kind='cubic')
+    xnew = np.linspace(min(prep_x), max(prep_x), 1000, endpoint=True)
+    plot(xnew+min(x),f(xnew), linestyle='-', linewidth=2, color = 'red')
+    '''
+    # plot graph of standart voltage
+    if plot_standart:
+        standart_voltages = [220*0.9, 220, 220*1.1]
+        for voltage in standart_voltages:
+            hlines(voltage, min(x), max(x), linestyle='--', color = 'orangered')
     # decoration
-    plt.title("Данные о напряжении за период: " +  
-            datetime.strftime(min(file_dates), "%d/%m/%Y") + ' - ' +   # начальная дата из данных
-            datetime.strftime(max(file_dates), "%d/%m/%Y"), size ='xx-large')   # конечная дата из данных
-    plt.grid(True, linestyle='-', color='0.75') #сетка
+    title("Данные о напряжении за период: " +  
+            datetime.strftime(min(file_dates), "%d/%m/%Y") + ' - ' +   # first date from file
+            datetime.strftime(max(file_dates), "%d/%m/%Y"), size ='xx-large')   # last date from file
+    grid(True, linestyle='-', color='0.75')
     # Х axis
-    plt.xlabel("Дата", size = 'x-large')
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%y %H:%M'))   #форматирование в формате даты
-    plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval = 1))  # интервал - 1 час
-    plt.gcf().autofmt_xdate()
-    plt.xticks(ha ='center', rotation=60)  
-    plt.xlim(min(x), max(x))
+    xlabel("Дата", size = 'x-large')
+    gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%y %H:%M'))   # format ticks 
+    gca().xaxis.set_major_locator(mdates.HourLocator(interval = 1))  # interval of ticks - 1 hour
+    gcf().autofmt_xdate()
+    xticks(ha ='center', rotation=60)  
+    xlim(min(x), max(x))
     # Y axis
-    plt.ylabel("Напряжение в сети, В", size ='x-large')
-    plt.yticks( np.arange(200, np.floor(max(y)), 1), rotation='horizontal')
-    plt.ylim(200, np.ceil(max(y)))
+    ylabel("Напряжение в сети, В", size ='x-large')
+    yticks( np.arange(195, 245, 1), rotation='horizontal')
+    ylim(195, 245)
     # saving file
     if (fname): # if name != None
-        plt.subplots_adjust(left=0.01, bottom=0.07, right=0.99, top=0.97)
-        figure = plt.gcf() # choose current graph
+        subplots_adjust(left=0.01, bottom=0.07, right=0.99, top=0.97)
+        figure = gcf() # choose current graph
         figure.set_size_inches(len(x_dates)/6, 20) 
-        plt.savefig(os.path.join(CHART_DIR, fname), dpi = 72) 
+        savefig(os.path.join(CHART_DIR, fname), dpi = 72) 
     # adjust and fullscreen
-    plt.subplots_adjust(left=0.04, bottom=0.12, right=0.99, top=0.97)
-    plt.get_current_fig_manager().full_screen_toggle() # (f on keyboard)
-    plt.show()
+    subplots_adjust(left=0.04, bottom=0.12, right=0.99, top=0.97)
+    get_current_fig_manager().full_screen_toggle() # (f on keyboard)
+    show()
 
 #parce data from txt -> 0 column - dates, 3 column - Line Voltage
 file_dates, y_prices = np.loadtxt(
@@ -53,5 +65,5 @@ file_dates, y_prices = np.loadtxt(
 x_dates = mdates.date2num(file_dates)   # format data objects to number of days from 0001-01-01
 y = y_prices.astype(float)   # format to float
 # output
-file_name = "all.png"
-plot_graph(x_dates, y, fname=file_name)
+file_name = None # "all.png"
+plot_graph(x_dates, y, plot_standart = True, fname = file_name)
